08804f9a644c96e2652e6dcf80fa62209413ddd7
root <345632369@qq.com>
2018-06-27 15:26:29 +0800

    修改

--- app/Http/Controllers/Wechat/WechatController.php
+++ app/Http/Controllers/Wechat/WechatController.php
@@ -9,10 +9,16 @@ class WechatController extends Controller
 {
     public function mini(Request $request){
         $code = $request->code;
+        $iv = $request->iv;
+        $encryptedData = $request->encryptedData;
         if (!isset($code)){
             return response('參數錯誤');
         }
         $app = app('wechat.mini_program');
-        $app->auth->session($code);
+        $res = $app->auth->session($code);
+
+        $decryptedData = $app->encryptor->decryptData($res['session_key'], $iv, $encryptedData);
+        dd($decryptedData);
+        return $res;
     }
 }
