93c0318ff08522c595f6c01693fc78c656f9b6b4
大头蔡\greeny <greenycaicai@163.com>
2018-08-10 23:08:57 +0800

    更新订单详情接口

--- app/Repositories/Order/OrderRepository.php
+++ app/Repositories/Order/OrderRepository.php
@@ -125,10 +125,9 @@ class OrderRepository implements OrderRepositoryInterface
     public function getOrderDetail($order_id)
     {
         try{
-            $order = Order::select('order_headers.*','color','quantity','combo_id','ol.unit_price','good_id','thumbnail','nick_name','buyer_msg')
+            $order = Order::select('order_headers.*','color','quantity','combo_id','unit_price','good_id','nick_name','buyer_msg')
                 ->leftJoin('order_lines as ol','order_headers.uid','=','ol.header_id')
                 ->leftJoin('clients','clients.id','=','order_headers.client_id')
-                ->leftJoin('goods','goods.uid','=','good_id')
                 ->where('order_headers.uid',$order_id)->first();
 
             //订单状态，见xm_lookup_values表ORDER_STATUS：0-已下单，1-已支付，2-待发货，3-已发货，4-已完成，5-异常，6-申请退货，7-确认退货，8-已退货
